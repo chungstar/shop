4804ed2965180d221d4abdbbacc1a77ab4d91357
정현규 <gusrb0511@gmail.com>
2022-12-31 01:54:49 +0900

    카드 컴포넌트

--- src/App.js
+++ src/App.js
@@ -23,24 +23,25 @@ function App() {
       <div className='main-bg' style={{ backgroundImage : 'url('+ bg +')' }}></div>
 
       <Row>
-        <Col sm>
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-          <h4>{shoes[0].title}</h4>
-          <p>{shoes[0].content}</p>
-        </Col>
-        <Col sm>
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].content}</p>
-        </Col>
-        <Col sm>
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-              <h4>{shoes[2].title}</h4>
-              <p>{shoes[2].content}</p>
-        </Col>
+        {
+          shoes.map((a, i)=>{
+            return(
+              <Card shoes={shoes[i]} i={i+1}/>
+            )
+          })
+        }
       </Row>
     </div>
   );
 }
 
+function Card(props){
+  return (
+    <Col sm>
+      <img src={"https://codingapple1.github.io/shop/shoes"+ props.i +".jpg"} width="80%"/>
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+    </Col>
+  )
+}
 export default App;
