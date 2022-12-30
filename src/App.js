c2b81044f331213c2c3265fc0a334a7b0b586980
정현규 <gusrb0511@gmail.com>
2022-12-31 01:05:09 +0900

    modified:   src/App.js 	new file:   src/data.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
 import './App.css';
 import bg from './img/bg.png';
+import data from './data.js'
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
@@ -20,18 +25,18 @@ function App() {
       <Row>
         <Col sm>
           <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-          <h4>상품명</h4>
-          <p>상품설명</p>
+          <h4>{shoes[0].title}</h4>
+          <p>{shoes[0].content}</p>
         </Col>
         <Col sm>
           <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content}</p>
         </Col>
         <Col sm>
           <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-              <h4>상품명</h4>
-              <p>상품설명</p>
+              <h4>{shoes[2].title}</h4>
+              <p>{shoes[2].content}</p>
         </Col>
       </Row>
     </div>
