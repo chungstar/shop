083af6169b335b70d8e4077309ffb2f3ee56d8f2
정현규 <gusrb0511@gmail.com>
2023-01-06 23:26:01 +0900

    lifecycle useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,37 +1,42 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import styled from "styled-components";
-
-let YellowBtn = styled.button`
-  background : ${ props=>props.bg };
-  color : balck;
-  padding : 10px;
-`
 
 
 function Detail(props){
-
-    let {id} = useParams();
-    let item = props.shoes.find(function(x){
-        return x.id == id
-      });
     
-      return (
-        <div className="container">
-          <YellowBtn bg="blue" >버튼</YellowBtn>
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">{item.title}</h4>
-              <p>{item.content}</p>
-              <p>{item.price}원</p>
-              <button className="btn btn-danger">주문하기</button> 
-            </div>
+  useEffect(()=>{
+
+  })
+
+
+  let [count, setCount]=useState(0)
+
+  let {id} = useParams();
+  let item = props.shoes.find(function(x){
+      return x.id == id
+    }); 
+  
+    return (
+      <div className="container">
+        <div className="alert alert-warning">
+          2초 이내 구매시 할인
+        </div>
+        { count }
+        <button onClick={()=>{ setCount(count+1) }}>?</button>
+        <div className="row">
+          <div className="col-md-6">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
           </div>
-      </div>  
-      )
-    };
+          <div className="col-md-6 mt-4">
+            <h4 className="pt-5">{item.title}</h4>
+            <p>{item.content}</p>
+            <p>{item.price}원</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
+        </div>
+    </div>  
+    )
+  };
     
    
   export default Detail
