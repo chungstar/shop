190eb5cb9f19e2b2018534badd4e6f3139367b70
정현규 <gusrb0511@gmail.com>
2023-01-07 13:19:10 +0900

    useEffect2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,30 +3,43 @@ import { useParams } from "react-router-dom"
 
 
 function Detail(props){
-    
-  useEffect(()=>{
-
-  })
-
-
+  
   let [count, setCount]=useState(0)
-
+  let [alert, setAlert]=useState(true)
+  let [num, setNum]=useState('')
   let {id} = useParams();
   let item = props.shoes.find(function(x){
       return x.id == id
-    }); 
+    });
+
+  useEffect(()=>{
+    let a = setTimeout(()=>{ setAlert(false) },2000)
+    
+    return ()=>{
+      clearTimeout(a)
+    }
+    },[])
+    useEffect(()=>{
+      if (isNaN(num) == true){
+        alert('그러지마세요')
+      }
+    }, [num])
   
     return (
       <div className="container">
-        <div className="alert alert-warning">
-          2초 이내 구매시 할인
-        </div>
-        { count }
+        
+        { alert == true
+        ? <div className="alert alert-warning">
+            2초 이내 구매시 할인
+          </div>
+        : null }
+        {count}
         <button onClick={()=>{ setCount(count+1) }}>?</button>
         <div className="row">
           <div className="col-md-6">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
           </div>
+          <input onChange={(e)=>{ setNum(e.target.value) }} />
           <div className="col-md-6 mt-4">
             <h4 className="pt-5">{item.title}</h4>
             <p>{item.content}</p>
