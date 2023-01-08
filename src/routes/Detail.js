7c1ffb2eb2afea336e5b322c2b13c8673b4255f1
정현규 <gusrb0511@gmail.com>
2023-01-08 16:17:53 +0900

    탭ui

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-
+import { Nav } from "react-bootstrap"
 
 function Detail(props){
   
@@ -8,6 +8,7 @@ function Detail(props){
   let [alert, setAlert]=useState(true)
   let [num, setNum]=useState('')
   let {id} = useParams();
+  let [탭,탭변경] = useState(0)
   let item = props.shoes.find(function(x){
       return x.id == id
     });
@@ -47,9 +48,23 @@ function Detail(props){
             <button className="btn btn-danger">주문하기</button> 
           </div>
         </div>
+        <Nav variant="tabs"  defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link onClick={()=>{ 탭변경(0) }} eventKey="link0">버튼0</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>{ 탭변경(1) }} eventKey="link1">버튼1</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>{ 탭변경(2) }} eventKey="link2">버튼2</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <TabContent 탭={탭}/>
     </div>  
     )
-  };
-    
-   
-  export default Detail
+  }
+  function TabContent({탭}){
+    return [<div>내용0</div>,<div>내용1</div>,<div>내용2</div>][탭]
+  }
+
+  export default Detail;
